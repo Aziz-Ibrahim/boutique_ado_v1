d03ffcb754d00befb93f8c487350e50f1ef9e034
Aziz-Ibrahim <dsnxc622@students.codeinstitute.net>
2025-05-14 23:50:05 +0100

    Set up AWS storage for static and media files

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -55,8 +55,9 @@ INSTALLED_APPS = [
     'checkout',
     'profiles',
 
-    # 'crispy_forms',
+    # Other
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -181,6 +182,25 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    # Bucket Settings
+    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-ai'
+    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and Media Files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    MEDIAFILES_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
 
 # Stripe
 FREE_DELIVERY_THRESHOLD = 50
